243d0b872d9c26830a333dadec86c57f19de2491
Nikolai <nikolai.nagornyh@gmail.com>
2022-08-16 12:02:05 +0300

    done first task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TreeItem, TreeItems} from "./store/TreeItemStore";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-first-project-innowaise';
+export class AppComponent implements OnInit{
+  items: TreeItem[] = TreeItems;
+
+  ngOnInit(): void {
+
+  }
+
+  calc(currentWidth: string):string {
+    return parseInt(currentWidth)-100+"px";
+  }
+
 }
---
+++ src/app/store/TreeItemStore.ts
@@ -0,0 +1,64 @@
+
+export interface TreeItem {
+  id: string;
+  name:string;
+  children: TreeItem[]
+}
+
+
+export const TreeItems: TreeItem[] = [
+  {
+    id: '1',
+    name: 'Den',
+    children: []
+  },
+  {
+    id: '2',
+    name: 'Den2',
+    children: [
+      {
+        id: '4',
+        name: 'Den2Child1',
+        children: []
+      },
+      {
+        id: '5',
+        name: 'Den2Child2',
+        children: []
+      }
+    ]
+  },
+  {
+    id: '3',
+    name: 'Den3',
+    children: [
+      {
+        id: '6',
+        name: 'Den3Child1',
+        children: []
+      },
+      {
+        id: '7',
+        name: 'Den3Child2',
+        children: [
+          {
+            id: '8',
+            name: 'Den3Child2_1',
+            children: []
+          },
+          {
+            id: '9',
+            name: 'Den3Child2_2',
+            children: []
+          }
+        ]
+      },
+      {
+        id: '10',
+        name: 'Den3Child3',
+        children: []
+      }
+    ]
+  }
+];
+
